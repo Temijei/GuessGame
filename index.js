83e230387c570eafa9cd7cac29433c29018f0aa7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-16 14:27:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const randomNumber = Math.floor(Math.random() * 15) + 1
-    console.log('Random Number', randomNumber)
+const randomNumber = Math.floor(Math.random() * 15) + 1;
+// console.log('Random Number', randomNumber);
 
-    function checkGuess() {
-      let myGuess = guess.value
-      if (myGuess === randomNumber) {
-        feedback.textContent = "You got it right!✔👏"
-      } else if (myGuess > randomNumber) {
-        feedback.textContent = "Your guess is " + myGuess + ". That's too high.😢"
-      } else if (myGuess < randomNumber) {
-       feedback.textContent = "Your guess is " + myGuess + ". That's too low.😢"
-     }
-   }
-   submitGuess.addEventListener('click', checkGuess)
+let Guess = document.querySelector('.guess');
+let UserScore = document.querySelector('.user-score').textContent;
+// console.log(Guess.value);
+
+// Default User score
+console.log(UserScore);
+
+function checkGuess() {
+	let UserGuess = Guess.value;
+
+	if (UserGuess == randomNumber) {
+		feedback.textContent = 'You got it right!✔👏';
+	} else if (UserGuess > randomNumber) {
+		feedback.textContent =
+			'Your guess is ' + UserGuess + ". That's too high.😢";
+	} else if (UserGuess < randomNumber) {
+		feedback.textContent =
+			'Your guess is ' + UserGuess + ". That's too low.😢";
+	}
+}
